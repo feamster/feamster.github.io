35c8b6172e9fb34096d3e2815f21ea94b0243972
Nick Feamster <feamster@uchicago.edu>
2026-07-03 18:39:04 -0500

    Bump bib cleanup, refresh cv.pdf, render DOI URLs with [DOI] link

--- js/bib-list.js
+++ js/bib-list.js
@@ -2560,6 +2560,9 @@ var bibtexify = (function($) {
             if (entryData.url && entryData.url.match(/.*\.pdf/)) {
                 itemStr += ' <a title="PDF-version of this article"  target="_blank" href="' +
                             entryData.url + '"><i class="fa fa-file-pdf-o"></i><\/a>';
+            } else if (entryData.url && entryData.url.match(/doi\.org\//i)) {
+                itemStr += ' <a title="DOI (canonical link)" target="_blank" href="' + entryData.url +
+                            '">[DOI]<\/a>';
             } else if (entryData.url) {
                 itemStr += ' <a title="This article online" target="_blank" href="' + entryData.url +
                             '"><i class="fa fa-globe""></i><\/a>';
